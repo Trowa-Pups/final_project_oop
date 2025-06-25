a2c7a13bb53d155a290f28dc18628d233624e4b1
unknown <sainttrowagmail.com>
2025-06-25 22:44:45 +0800

    Added the turn counter and lifespan counter from the last project and it took a while because my git bash crashed and vscode was glitchy

--- board.py
+++ board.py
@@ -6,4 +6,23 @@ class Board():
                       "-", "-", "-"]  
         
         self.turn_coutner = [0] * 9
-        
+
+    def display_board(self):
+        print(self.board[0] + " | " + self.board[1] + " | " + self.board[2] + "\n"
+          + "---------" + "\n"
+          + self.board[3] + " | " + self.board[4] + " | " + self.board[5] + "\n"
+          + "---------" + "\n"
+          + self.board[6] + " | " + self.board[7] + " | " + self.board[8])
+    
+    def update_position(self, position, current_player): #This part is where it counts what lifespan of the symbol
+        self.board[position] = current_player
+        self.board[position] = 6
+
+    def turn_counter(self):
+        for i in range(9):
+            if self.turn_counter > 0:
+                self.turn_counter -= 1
+                if self.turn_counter == 0:
+                    self.board = "-"
+
+
