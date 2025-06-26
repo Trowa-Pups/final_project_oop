8451aa19ec3df59b24e846640bfb729a295b8369
unknown <sainttrowagmail.com>
2025-06-26 18:59:16 +0800

    Made this file for the player and input systems in the project, and also copied and modified the systems in the last project

---
+++ player.py
@@ -0,0 +1,19 @@
+#This is where the player and input part of the project is
+class Player:
+    def __init__(self, symbol):  #Making this so that it can be both X and O
+        self.symbol = symbol
+    
+    def user_inputs(self, board): #Trying to copy the project so i put self and board just in case
+        while True:
+            try:
+                user_input = int(input("Enter a number between 1 to 9!")) - 1 #Putting - 1 here so i dont need to make it [user_input - 1]
+
+                if 0 <= user_input <= 8 and board[user_input] == "-":
+                    return user_input #To store the player's input value
+            
+                else:
+                    print("Invalid Move! Please try again")
+
+            except ValueError:
+                print("Invalid Value! Please input 1 to 9")
+        
