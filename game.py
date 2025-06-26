d0170b4869830fc4da59a31943e3aa428740b2d9
unknown <sainttrowagmail.com>
2025-06-26 23:18:54 +0800

    Now im starting to put the play so that it can properly play or start the program

--- game.py
+++ game.py
@@ -7,3 +7,11 @@ class TicTacToe:
         self.current_player = player_1
         self.other_player = player_2
         self.winner = None
+    
+    def switch_player(self): #To switch the players in their turns
+        self.current_player , self.other_player = self.other_player, self.current_player
+
+    def play(self):
+        self.board.display()
+        move = self.current_player.get_input(self.board)
+        self.board.update_position(move, self.current_player.symbol)
