0b7fd0e3fec2e418395993eed729b40340c41d02
unknown <sainttrowagmail.com>
2025-06-27 18:20:24 +0800

    A little bit of fixing but still dont know what the error is

--- win_checker.py
+++ win_checker.py
@@ -1,14 +1,14 @@
 #This file checks the board if a specific symbol has a win condition
 class WinChecker:
     def win_check(self, board, symbol):
-        win_conditions = [[board[0], board[1], board[2]] #Horizontal
-                          [board[3], board[4], board[5]]
-                          [board[6], board[7], board[8]]
-                          [board[0], board[3], board[6]] #Vertical
-                          [board[1], board[4], board[7]]
-                          [board[2], board[5], board[8]]
-                          [board[0], board[4], board[8]] #Diagonal
-                          [board[2], board[4], board[6]]
+        win_conditions = [[board[0], board[1], board[2]], #Horizontal
+                          [board[3], board[4], board[5]],
+                          [board[6], board[7], board[8]],
+                          [board[0], board[3], board[6]], #Vertical
+                          [board[1], board[4], board[7]],
+                          [board[2], board[5], board[8]],
+                          [board[0], board[4], board[8]], #Diagonal
+                          [board[2], board[4], board[6]],
                           ]
         
         for condition in win_conditions: #checks if one of the conditions are met
