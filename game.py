dfdc8c15ac005151c5ab8a6148372fff0a828d1c
unknown <sainttrowagmail.com>
2025-06-27 18:35:43 +0800

    im pretty sure i got rid most of the errors, but not 100%sure

--- game.py
+++ game.py
@@ -9,17 +9,18 @@ class TicTacToe:
         self.current_player = player_1
         self.other_player = player_2
         self.winner = None
-    
+
     def switch_player(self): #To switch the players in their turns
         self.current_player , self.other_player = self.other_player, self.current_player
 
     def play(self): #Player turns
         while True:
+            win_checker = WinChecker()
             self.board.display_board()
             move = self.current_player.user_inputs(self.board)
             self.board.update_position(move, self.current_player.symbol)
 
-            if win_check(self.board, self.current_player.symbol): #To show winner 
+            if win_checker.win_check(self.board, self.current_player.symbol): #To show winner 
                 self.board.display_board()
                 print(f"The winner is {self.current_player.symbol}!")
                 break
