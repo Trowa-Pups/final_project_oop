d22475497af287f7fec32e7ffc080e894cb2934f
unknown <sainttrowagmail.com>
2025-06-27 19:51:28 +0800

    I made the board bigger

--- board.py
+++ board.py
@@ -8,12 +8,12 @@ class Board():
         
         self.turns = [0] * 9 #Changed it to this so that its not the same as the def turn_counter
 
-    def display_board(self):
-        print(self.board[0] + " | " + self.board[1] + " | " + self.board[2] + "\n"
-          + "---------" + "\n"
-          + self.board[3] + " | " + self.board[4] + " | " + self.board[5] + "\n"
-          + "---------" + "\n"
-          + self.board[6] + " | " + self.board[7] + " | " + self.board[8])
+    def display_board(self): #Added more spaces to make it more big
+        print(self.board[0] + "  |  " + self.board[1] + "  |  " + self.board[2] + "\n" +"\n"
+          + "--------------" + "\n" + "\n"
+          + self.board[3] + "  |  " + self.board[4] + "  |  " + self.board[5] + "\n" + "\n"
+          + "--------------" + "\n" + "\n"
+          + self.board[6] + "  |  " + self.board[7] + "  |  " + self.board[8])
     
     def update_position(self, position, current_player): #This part is where it counts what lifespan of the symbol
         self.board[position] = current_player
