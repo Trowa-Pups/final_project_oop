4b0fae768b6e322b65a8f91e33e3a4ef2d7639e0
unknown <sainttrowagmail.com>
2025-06-28 08:22:54 +0800

    Small tweaks

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player:
     def user_inputs(self, board): #Trying to copy the project so i put self and board just in case
         while True:
             try:
-                user_input = int(input(Fore.CYAN + "Enter a number between 1 to 9!: " + Style.RESET_ALL)) - 1 #Putting - 1 here so i dont need to make it [user_input - 1]
+                user_input = int(input("\n" + Fore.CYAN + "Enter a number between 1 to 9!: " + Style.RESET_ALL)) - 1 #Putting - 1 here so i dont need to make it [user_input - 1]
 
                 if 0 <= user_input <= 8 and board.board[user_input] == "-":
                     return user_input #To store the player's input value
@@ -19,6 +19,6 @@ class Player:
                 print("Invalid Value! Please input 1 to 9")
 
 class OtherPlayer(Player): #For Polymorphism 
-    def get_input(self, board):
-        return super().user_input(board)
+    def user_inputs(self, board):
+        return super().user_inputs(board)
         
